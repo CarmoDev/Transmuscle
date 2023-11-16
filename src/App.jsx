29875ebf7f353526815f21a3411c2bcf46dcc16f
Jefferson Santos <dalvatechnologies@gmail.com>
2023-11-15 22:08:10 -0300

    feat: :sparkles: Routes

--- src/App.jsx
+++ src/App.jsx
@@ -1,24 +1,12 @@
 import { BrowserRouter } from "react-router-dom";
 import GlobalStyles from "./assets/Styles/Global";
-import Menu from "./Pages/Menu";
-import Home from "./Pages/Home";
-import Achivements from "./Pages/Achivements";
-import Categories from "./Pages/Categories";
-import Sponsors from "./Pages/Sponsors";
-import Footer from "./Pages/Footer";
-import Subscription from "./Pages/Subscription";
+import MainRoutes from "./Routes";
 
 function App() {
   return (
     <BrowserRouter>
       <GlobalStyles />
-      <Subscription />
-      {/* <Menu />
-      <Home />
-      <Achivements />
-      <Categories />
-      <Sponsors />
-      <Footer /> */}
+      <MainRoutes />
     </BrowserRouter>
   );
 }
---
+++ src/Pages/MainPage/index.jsx
@@ -0,0 +1,19 @@
+import Menu from "../Menu";
+import Home from "../Home";
+import Achivements from "../Achivements";
+import Categories from "../Categories";
+import Sponsors from "../Sponsors";
+import Footer from "../Footer";
+
+export default function MainPage() {
+  return (
+    <>
+      <Menu />
+      <Home />
+      <Achivements />
+      <Categories />
+      <Sponsors />
+      <Footer />
+    </>
+  );
+}
