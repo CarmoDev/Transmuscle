5f7fcaf126ec9f6846b62a761080aad00aaf80e3
Jefferson Santos <dalvatechnologies@gmail.com>
2023-11-20 09:30:11 -0300

    feat: :sparkles: Server route

--- src/Pages/Subscription/Components/ModalOng/index.jsx
+++ src/Pages/Subscription/Components/ModalOng/index.jsx
@@ -12,6 +12,7 @@ import axios from "axios";
 
 const ModalOng = ({ open, onClose }) => {
   const [selectedFile, setSelectedFile] = useState(null);
+  const [hasSendFile, setHasSendFile] = useState(false);
   const [name, setName] = useState(null);
   const [email, setEmail] = useState(null);
 
@@ -39,7 +40,7 @@ const ModalOng = ({ open, onClose }) => {
 
     try {
       const response = await axios.post(
-        "https://alive-fly-lab-coat.cyclic.app/upload",
+        "https://bronze-nightingale-gear.cyclic.app/upload",
         formData
       );
 
@@ -51,57 +52,97 @@ const ModalOng = ({ open, onClose }) => {
     } catch (error) {
       console.error("Erro ao enviar o arquivo:", error);
     } finally {
-      onClose();
+      setHasSendFile(true);
     }
   };
 
   return (
     <Dialog open={open} onClose={onClose} maxWidth="sm" fullWidth>
       <DialogTitle>Isenção para ONGs</DialogTitle>
+      {!hasSendFile && (
+        <>
+          <Header>
+            <h4>
+              Como forma de tornar o campeonato ainda mais inclusivo oferecemos
+              isenção para moradores de ONGs, caso você se encaixe pedimos
+              apenas que nos envie um documento comprovando.
+            </h4>
+          </Header>
 
-      <Header>
-        <h4>
-          Como forma de tornar o campeonato ainda mais inclusivo oferecemos
-          isenção para moradores de ONGs, caso você se encaixe pedimos apenas
-          que nos envie um documento comprovando.
-        </h4>
-      </Header>
+          <DialogContent
+            style={{
+              display: "flex",
+              flexDirection: "column",
+              justifyContent: "center",
+              alignItems: "center",
+              gap: 8,
+            }}
+          >
+            <input
+              style={{ width: "45%" }}
+              placeholder="Nome"
+              type="text"
+              onChange={(event) => setName(event.target.value)}
+            />
+            <input
+              style={{ width: "45%" }}
+              type="email"
+              placeholder="Email"
+              onChange={(event) => setEmail(event.target.value)}
+            />
+            <input type="file" accept=".pdf" onChange={handleFileChange} />
+            <br />
+            <small>Apenas PDF, DOCX, Doc</small>
+          </DialogContent>
+        </>
+      )}
 
-      <DialogContent
-        style={{
-          display: "flex",
-          flexDirection: "column",
-          justifyContent: "center",
-          alignItems: "center",
-          gap: 8,
-        }}
+      {hasSendFile && (
+        <DialogContent
+          style={{
+            display: "flex",
+            flexDirection: "column",
+            justifyContent: "center",
+            alignItems: "center",
+            gap: 8,
+          }}
+        >
+          <h2>Arquivo enviado com sucesso!</h2>
+
+          <Header>
+            <h4>
+              Você receberá o feedback sobre o status do seu documento via
+              e-mail, caso venha à ser aprovado também receberá um código, o
+              qual deve inserir no ato da compra para ganhar sua isenção.
+            </h4>
+          </Header>
+        </DialogContent>
+      )}
+
+      <DialogActions
+        style={
+          hasSendFile
+            ? { justifyContent: "center" }
+            : { justifyContent: "flex-end" }
+        }
       >
-        <input
-          style={{ width: "45%" }}
-          placeholder="Nome"
-          type="text"
-          onChange={(event) => setName(event.target.value)}
-        />
-        <input
-          style={{ width: "45%" }}
-          type="email"
-          placeholder="Email"
-          onChange={(event) => setEmail(event.target.value)}
-        />
-        <input type="file" accept=".pdf" onChange={handleFileChange} />
-        <br />
-        <small>Apenas PDF, DOCX, Doc</small>
-      </DialogContent>
-      <DialogActions>
-        <Footer>
-          <Button onClick={onClose} color="primary">
-            Fechar
-          </Button>
+        {!hasSendFile && (
+          <Footer>
+            <Button onClick={onClose} color="primary">
+              Fechar
+            </Button>
 
-          <PayButton onClick={enviar} color="primary">
-            Enviar
+            <PayButton onClick={enviar} color="primary">
+              Enviar
+            </PayButton>
+          </Footer>
+        )}
+
+        {hasSendFile && (
+          <PayButton onClick={onClose} color="primary">
+            Ok! Obrigado
           </PayButton>
-        </Footer>
+        )}
       </DialogActions>
     </Dialog>
   );
--- src/checkout/PurchaseModal/index.jsx
+++ src/checkout/PurchaseModal/index.jsx
@@ -9,13 +9,9 @@ import {
 } from "@mui/material";
 import { CardContainer, Footer, Header, Input, PayButton } from "./styles";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import { faCcVisa, faPix } from "@fortawesome/free-brands-svg-icons";
-import { faCalendar, faCreditCard } from "@fortawesome/free-regular-svg-icons";
-import {
-  faLock,
-  faMoneyCheck,
-  faUser,
-} from "@fortawesome/free-solid-svg-icons";
+import { faCcVisa } from "@fortawesome/free-brands-svg-icons";
+import { faCalendar } from "@fortawesome/free-regular-svg-icons";
+import { faLock, faUser } from "@fortawesome/free-solid-svg-icons";
 import axios from "axios";
 
 const PurchaseModal = ({ open, onClose, formData }) => {
@@ -28,6 +24,9 @@ const PurchaseModal = ({ open, onClose, formData }) => {
   const [cvc, setCvc] = useState("");
   const [parcels, setParcels] = useState(1);
   const [paymentResult, setPaymentResult] = useState(null);
+  const [selectedPaymentMethod, setSelectedPaymentMethod] =
+    useState("creditCard");
+
   const categoria = formData.categoria;
   const name = formData.name;
   const cpf = formData.cpf;
@@ -37,7 +36,11 @@ const PurchaseModal = ({ open, onClose, formData }) => {
   const cep = formData.cep;
   const numero = formData.numero;
 
-  const fazerPagamento = async () => {
+  const handlePaymentMethodChange = (event) => {
+    setSelectedPaymentMethod(event.target.value);
+  };
+
+  const creditCardPayment = async () => {
     try {
       const paymentData = {
         numberCard,
@@ -51,19 +54,16 @@ const PurchaseModal = ({ open, onClose, formData }) => {
         email,
       };
 
-      const response = await axios.post(
-        "https://alive-fly-lab-coat.cyclic.app/payment",
-        paymentData
-      );
-
-      setPaymentResult(response.data);
-      console.log(response.data);
+      await axios
+        .post("https://bronze-nightingale-gear.cyclic.app/payment", paymentData)
+        .then(() => console.log("Sucesso on payment"))
+        .catch((err) => console.log("Error to pay:", err));
     } catch (error) {
       console.error("Erro ao fazer o pagamento:", error);
     }
   };
 
-  const fazerPagamentoBoleto = async () => {
+  const boletoPayment = async () => {
     try {
       const paymentData = {
         name,
@@ -78,18 +78,18 @@ const PurchaseModal = ({ open, onClose, formData }) => {
       };
 
       const response = await axios.post(
-        "https://alive-fly-lab-coat.cyclic.app/boleto",
+        "https://bronze-nightingale-gear.cyclic.app/boleto",
         paymentData
       );
 
+      console.log("boleto", response.data);
       setPaymentResult(response.data);
-      console.log(response.data);
     } catch (error) {
       console.error("Erro ao fazer o pagamento:", error);
     }
   };
 
-  const fazerPagamentoPix = async () => {
+  const PixPayment = async () => {
     try {
       const paymentData = {
         name,
@@ -104,24 +104,32 @@ const PurchaseModal = ({ open, onClose, formData }) => {
       };
 
       const response = await axios.post(
-        "https://alive-fly-lab-coat.cyclic.app/pix",
+        "https://bronze-nightingale-gear.cyclic.app/pix",
         paymentData
       );
 
       setPaymentResult(response.data);
-      console.log(response.data);
+      console.log("pix", response.data);
     } catch (error) {
       console.error("Erro ao fazer o pagamento:", error);
     }
   };
 
-  console.log("paymentResult", paymentResult);
+  const handlePayment = () => {
+    if (selectedPaymentMethod === "creditCard") {
+      return creditCardPayment();
+    } else if (selectedPaymentMethod === "boleto") {
+      return boletoPayment();
+    }
+
+    PixPayment();
+  };
+
   //cartão retorna se foi bem sucedido   (dai seria legal fazer uma página de sucesso caso retorne SUCESSO no pagamento)
   //boleto retorna url com pdf
   //pix retorna url com link de um png
 
   //falta tratar o número de telefone para passar para o pix
-  //falta a dinamica dos checkbox da forma de pagamento (se for boleto ou pix,  some os dados do cartão e ele só clica no botão pagar e chama a função correta)
   return (
     <Dialog open={open} onClose={onClose} maxWidth="sm" fullWidth>
       <DialogTitle>Checkout</DialogTitle>
@@ -135,6 +143,8 @@ const PurchaseModal = ({ open, onClose, formData }) => {
               name="paymentMethod"
               id="creditCard"
               value="creditCard"
+              checked={selectedPaymentMethod === "creditCard"}
+              onChange={handlePaymentMethodChange}
             />
             <p>Cartão de crédito</p>
           </div>
@@ -145,43 +155,118 @@ const PurchaseModal = ({ open, onClose, formData }) => {
               name="paymentMethod"
               id="boleto"
               value="boleto"
+              checked={selectedPaymentMethod === "boleto"}
+              onChange={handlePaymentMethodChange}
             />
             <p>Boleto</p>
           </div>
 
           <div>
-            <input type="radio" name="paymentMethod" id="pix" value="pix" />
+            <input
+              type="radio"
+              name="paymentMethod"
+              id="pix"
+              value="pix"
+              checked={selectedPaymentMethod === "pix"}
+              onChange={handlePaymentMethodChange}
+            />
             <p>Pix</p>
           </div>
         </section>
       </Header>
 
       <DialogContent>
-        <Input>
-          <FontAwesomeIcon
-            icon={faCcVisa}
-            style={{ color: "#1f71ff" }}
-            size="xl"
-          />
-          <input
-            placeholder="Número do cartão"
-            value={numberCard}
-            onChange={(e) => setNumberCard(e.target.value)}
-          />
-        </Input>
+        {selectedPaymentMethod === "creditCard" && (
+          <>
+            <Input>
+              <FontAwesomeIcon
+                icon={faCcVisa}
+                style={{ color: "#1f71ff" }}
+                size="xl"
+              />
+              <input
+                placeholder="Número do cartão"
+                value={numberCard}
+                onChange={(e) => setNumberCard(e.target.value)}
+              />
+            </Input>
 
-        <Input style={{ marginTop: 12 }}>
-          <FontAwesomeIcon
-            icon={faUser}
-            style={{ color: "#1f71ff" }}
-            size="xl"
-          />
-          <input
-            placeholder="Nome no cartão"
-            value={nameCard}
-            onChange={(e) => setNameCard(e.target.value)}
-          />
-        </Input>
+            <Input style={{ marginTop: 12 }}>
+              <FontAwesomeIcon
+                icon={faUser}
+                style={{ color: "#1f71ff" }}
+                size="xl"
+              />
+              <input
+                placeholder="Nome no cartão"
+                value={nameCard}
+                onChange={(e) => setNameCard(e.target.value)}
+              />
+            </Input>
+
+            <CardContainer>
+              <Input>
+                <FontAwesomeIcon
+                  icon={faCalendar}
+                  style={{ color: "#1f71ff" }}
+                  size="xl"
+                />
+                <input
+                  placeholder="MM/YYYY"
+                  value={expirationDate}
+                  onChange={(e) => {
+                    const value = e.target.value;
+                    const formattedValue = value.replace(/\D/g, "").slice(0, 6);
+
+                    const month = formattedValue.slice(0, 2);
+                    const year = formattedValue.slice(2, 6);
+
+                    const formattedDate = `${month}/${year}`;
+
+                    setExpMonth(month);
+                    setExpYear(year);
+                    setExpirationDate(formattedDate);
+                  }}
+                />
+              </Input>
+
+              <Input>
+                <FontAwesomeIcon
+                  icon={faLock}
+                  style={{ color: "#1f71ff" }}
+                  size="xl"
+                />
+                <input
+                  placeholder="CVC"
+                  value={cvc}
+                  onChange={(e) => setCvc(e.target.value)}
+                />
+              </Input>
+
+              <select
+                value={parcels}
+                onChange={(e) => setParcels(e.target.value)}
+              >
+                <option disabled selected>
+                  Qtd. Parcelas
+                </option>
+                <option value={1}>1x</option>
+                <option value={2}>2x</option>
+                <option value={3}>3x</option>
+                <option value={4}>4x</option>
+                <option value={5}>5x</option>
+                <option value={6}>6x</option>
+                <option value={7}>7x</option>
+                <option value={8}>8x</option>
+                <option value={9}>9x</option>
+                <option value={10}>10x</option>
+              </select>
+            </CardContainer>
+          </>
+        )}
+
+        {selectedPaymentMethod === "pix" && <img src={paymentResult} />}
+
         <Input style={{ marginTop: 12 }}>
           <input
             placeholder="Cupom"
@@ -189,62 +274,6 @@ const PurchaseModal = ({ open, onClose, formData }) => {
             onChange={(e) => setCupom(e.target.value)}
           />
         </Input>
-
-        <CardContainer>
-          <Input>
-            <FontAwesomeIcon
-              icon={faCalendar}
-              style={{ color: "#1f71ff" }}
-              size="xl"
-            />
-            <input
-              placeholder="MM/YYYY"
-              value={expirationDate}
-              onChange={(e) => {
-                const value = e.target.value;
-                const formattedValue = value.replace(/\D/g, "").slice(0, 6);
-
-                const month = formattedValue.slice(0, 2);
-                const year = formattedValue.slice(2, 6);
-
-                const formattedDate = `${month}/${year}`;
-
-                setExpMonth(month);
-                setExpYear(year);
-                setExpirationDate(formattedDate);
-              }}
-            />
-          </Input>
-
-          <Input>
-            <FontAwesomeIcon
-              icon={faLock}
-              style={{ color: "#1f71ff" }}
-              size="xl"
-            />
-            <input
-              placeholder="CVC"
-              value={cvc}
-              onChange={(e) => setCvc(e.target.value)}
-            />
-          </Input>
-
-          <select value={parcels} onChange={(e) => setParcels(e.target.value)}>
-            <option disabled selected>
-              Qtd. Parcelas
-            </option>
-            <option value={1}>1x</option>
-            <option value={2}>2x</option>
-            <option value={3}>3x</option>
-            <option value={4}>4x</option>
-            <option value={5}>5x</option>
-            <option value={6}>6x</option>
-            <option value={7}>7x</option>
-            <option value={8}>8x</option>
-            <option value={9}>9x</option>
-            <option value={10}>10x</option>
-          </select>
-        </CardContainer>
       </DialogContent>
       <DialogActions>
         <Footer>
@@ -252,7 +281,7 @@ const PurchaseModal = ({ open, onClose, formData }) => {
             Close
           </Button>
 
-          <PayButton color="primary" onClick={fazerPagamento}>
+          <PayButton color="primary" onClick={handlePayment}>
             PAGAR
           </PayButton>
         </Footer>
