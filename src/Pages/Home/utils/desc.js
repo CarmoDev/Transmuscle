7845ea132d2c005d25f649b119fb7766459c168b
Jefferson Santos <dalvatechnologies@gmail.com>
2023-11-23 20:22:59 -0300

    feat: :sparkles: Updated texts

--- src/Pages/Home/utils/desc.js
+++ src/Pages/Home/utils/desc.js
@@ -12,24 +12,23 @@ export default [
   },
   {
     title: "Objetivos",
-    description: `Somos o primeiro e único campeonato de fisiculturismo voltado
-    exclusivamente para o público transgênero e Não-Binárie no Brasil.
-    Ele busca promover a inclusão, a igualdade de oportunidades e a
-    celebração da diversidade de corpos dentro da comunidade trans. Além
-    disso, o evento tem como objetivo contribuir para a conscientização,
-    aceitação e empoderamento das pessoas transgênero e Não-Binárie,
-    utilizando o esporte como uma plataforma de destaque para suas
-    habilidades físicas.`,
+    description: `Estabelecer o "Trans Muscle" como um campeonato exclusivo para pessoas transgênero
+    no fisiculturismo.
+    Promover a visibilidade, aceitação e inclusão de pessoas trans no contexto do esporte.
+    Fornecer suporte físico, mental e emocional aos competidores transgênero, ajudando-os
+    em sua jornada de transformação física.
+    Sensibilizar e educar a comunidade fisiculturista e o público em geral sobre as questões
+    relacionadas à identidade de gênero e à inclusão.
+    `,
   },
   {
     title: "Impacto Esperado",
-    description: `Somos o primeiro e único campeonato de fisiculturismo voltado
-    exclusivamente para o público transgênero e Não-Binárie no Brasil.
-    Ele busca promover a inclusão, a igualdade de oportunidades e a
-    celebração da diversidade de corpos dentro da comunidade trans. Além
-    disso, o evento tem como objetivo contribuir para a conscientização,
-    aceitação e empoderamento das pessoas transgênero e Não-Binárie,
-    utilizando o esporte como uma plataforma de destaque para suas
-    habilidades físicas.`,
+    description: `Aumentar a representatividade e a inclusão de pessoas transgênero no fisiculturismo.
+    Promover um ambiente esportivo mais inclusivo e igualitário, eliminando barreiras e
+    estereótipos.
+    Contribuir para a conscientização e a educação sobre questões relacionadas à
+    identidade de gênero e à diversidade.
+    Melhorar a saúde e o bem-estar das pessoas transgênero, fornecendo suporte físico e
+    emocional durante sua jornada de transformação.`,
   },
 ];
