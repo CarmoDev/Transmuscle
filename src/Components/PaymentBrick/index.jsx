0b7ce4b581ac4d635a9639f8a71e13c31e067a71
Jefferson Santos <dalvatechnologies@gmail.com>
2024-01-22 19:51:45 -0300

    Amount and server

--- src/Components/PaymentBrick/index.jsx
+++ src/Components/PaymentBrick/index.jsx
@@ -16,6 +16,7 @@ export default function Checkout({ amount, athleteForm, file }) {
 
   const customization = {
     paymentMethods: {
+      bankTransfer: "all",
       ticket: "all",
       creditCard: "all",
     },
